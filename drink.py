ba24832b943365d2c9fd481de5eac7d540a98ca2
Bo <bopeng81@gmail.com>
2015-07-14 15:59:49 -0500

    added functionality of including drinks that need just n more ingredients

--- drink.py
+++ drink.py
@@ -47,6 +47,40 @@ def mongo_query(ingredients_list=[]):
     }
     ])
 
+def drinks_short_n(ingredients_list=[], n=1):
+    return collection.aggregate(
+        [
+            { "$project": { "ingredients.ingredient": 1, 
+                            "name": 1,
+                            "site_id": 1,
+                            "instructions": 1,
+                            "recognitions": 1,
+                            "glass_type": 1,
+                            "inBOnly": 
+                            { "$setDifference": ["$ingredients.ingredient", 
+                                                 ingredients_list] 
+                          }, 
+                        } 
+          },
+            {
+                "$match": { 
+                    "inBOnly": {"$size": 1}
+                }
+                
+            },
+            {
+                "$project": {
+                    "name": 1,
+                    "site_id": 1,
+                    "ingredients.ingredient":1,
+                    "glass_type": 1,
+                    "instructions": 1,
+                    "recognitions": 1,
+                    "_id": 0,
+                }
+            }
+        ]
+    )
 
 #pprint(drink_dict)
 
@@ -63,11 +97,41 @@ def viz_page():
     """
     #with open("index.html", 'r') as viz_file:
     #    return viz_file.read()
-    db_ingredients = ["sweet vermouth", 
-                        "gin", 
-                        "Campari", 
-                        "Cointreau", 
-                        "Scotch"]
+    db_spirits = sorted(["gin",
+                         "rum",
+                         "tequila",
+                         "vodka",
+                         "Scotch",
+                         "rye"], key=str.lower)
+    db_liqueurs = sorted(["Cointreau",
+                          "sweet vermouth",
+                          "dry vermouth",
+                          "Campari",
+                          "Midori"], key=str.lower)
+    db_mixers = ["tonic water",
+                 "soda",
+                 "ginger beer"]
+    db_juices = ["orange juice",
+                 "grapefruit juice",
+                 "fresh lime juice"]
+    db_bitters = ["Angustora bitters",
+                  "orange bitters",
+                  "Peychaud's bitters"]
+    db_garnishes = ["lemon",
+                    "lime",
+                    "orange"]
+    db_ingredients = [{"category": "Spirits",
+                       "ingredients": db_spirits}, 
+                      {"category": "Liqueurs",
+                       "ingredients": db_liqueurs},
+                      {"category": "Mixers",
+                       "ingredients": db_mixers},
+                      {"category": "Juices",
+                       "ingredients": db_juices},                      
+                      {"category": "Bitters",
+                       "ingredients": db_bitters},                     
+                      {"category": "Garnishes",
+                       "ingredients": db_garnishes}]
     return flask.render_template("index.html", db_ingredients = db_ingredients)
 
 # Get an example and return it's score from the predictor model
@@ -86,10 +150,11 @@ def subset():
     drink_dict_ids = [x["site_id"] for x in drink_dict["result"]]
     orig_length = len(drink_dict["result"])
 
-    extended_drink_list = ingredients_list + ["fresh lime juice", "sugar"]
-    extended_drink_dict = mongo_query(extended_drink_list)
-    extended_drink_dict["result"] = [x for x in extended_drink_dict["result"] 
-                                     if x["site_id"] not in drink_dict_ids]
+    #extended_drink_list = ingredients_list + ["fresh lime juice"]
+    extended_drink_dict = drinks_short_n(ingredients_list)
+
+    # extended_drink_dict["result"] = [x for x in extended_drink_dict["result"] 
+    #                                  if x["site_id"] not in drink_dict_ids]
     #import pdb; pdb.set_trace()
     new_length = len(extended_drink_dict["result"])
 
